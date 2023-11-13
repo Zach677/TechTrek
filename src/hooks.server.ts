94f0a22f627397b784b7151d19d43c21b264d169
Star <i@xingbest.xyz>
2023-11-13 15:06:49 +0800

    fix: delete hooks
    Signed-off-by: Star <i@xingbest.xyz>

--- src/hooks.server.ts
+++
@@ -1,6 +0,0 @@
-import type { Handle } from '@sveltejs/kit'
-
-export const handle = (async ({ event, resolve }) => {
-  console.log('url ---> ', event.url.pathname)
-  return await resolve(event)
-}) satisfies Handle
