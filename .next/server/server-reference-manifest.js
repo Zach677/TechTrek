8fd50f060b04a59ed5be744af9abbdb66fd2187c
Star <i@ssstttar.com>
2024-06-22 23:19:57 +0800

    🔥 remove all files
    Signed-off-by: Star <i@ssstttar.com>

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UIYhlTqmfionTP6JCueo+1xcoHhl0qlmfIJ2u3A9oag=\"\n}"
--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import Card from '../components/Card.svelte'
-</script>
-
-<Card title="Star" content="This is my personal website." />
