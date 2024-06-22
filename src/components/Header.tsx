f2e5cc799959fde13884e76bfe9f8b8c2a3d947f
Star <i@ssstttar.com>
2024-06-22 21:24:54 +0800

    🔥 remove heroicon
    Signed-off-by: Star <i@ssstttar.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { HomeIcon } from '@heroicons/react/solid'
 
 const Header: React.FC = () => (
-  <header className="bg-blue-600 text-white p-4 shadow-md">
-    <div className="container mx-auto flex justify-between items-center">
+  <header className="bg-blue-600 p-4 text-white shadow-md">
+    <div className="container mx-auto flex items-center justify-between">
       <h1 className="text-3xl font-bold">我的个人主页</h1>
       <nav>
         <a href="/" className="mx-2 text-lg hover:text-gray-200">
