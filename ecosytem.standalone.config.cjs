bb5dd641b3a1a52cbcac9a13550ead7e6d40b478
Zach <i@ssstttar.com>
2024-09-29 16:26:09 +0800

    🔧 chore(ci): add build
    Signed-off-by: Zach <i@ssstttar.com>

---
+++ ecosytem.standalone.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'TechTrek',
+      script: 'server.js',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        PORT: 2323,
+        NODE_ENV: 'production',
+        NEXT_SHARP_PATH: process.env.NEXT_SHARP_PATH,
+      },
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    },
+  ],
+}
